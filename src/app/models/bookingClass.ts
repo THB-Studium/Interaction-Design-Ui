7cbdcdbe734691755d6e7a2f87c6b68e6210092b
steveboris <b.steve189@hotmail.com>
2021-12-22 07:15:19 +0100

    modification to be done for the bookingclass model

--- src/app/models/bookingClass.ts
+++ src/app/models/bookingClass.ts
@@ -1,6 +1,6 @@
 export class BookingClass {
   public id: string;
-  public type: string;
+  public type: string; /** change this attribut to titel or name */
   public preis: number;
-  public reiseAngebotId: string;
+  public reiseAngebotId: string; /** to be removed. No need. Instead change it to description.*/
 }
