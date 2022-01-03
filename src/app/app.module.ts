b6076e27864cdb67ecdbb005077a37a3825d4734
Meriem Amdouni <amdounimaryem94@gmail.com>
2022-01-03 11:07:37 +0100

    add-carousel-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
+import { CarouselComponent } from './pages/carousel/carousel.component';
 
 @NgModule({
   imports: [
@@ -31,7 +32,8 @@ import { ComponentsModule } from './components/components.module';
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    AuthLayoutComponent
+    AuthLayoutComponent,
+    CarouselComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
