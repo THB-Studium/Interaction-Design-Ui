5541d753d2dc60aa9af1303d3a70ed2118698ec4
Meriem Amdouni <amdounimaryem94@gmail.com>
2022-01-08 12:03:06 +0100

    add router

--- src/app/layouts/auth-layout/auth-layout.routing.ts
+++ src/app/layouts/auth-layout/auth-layout.routing.ts
@@ -1,11 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { AboutusComponent } from "src/app/pages/aboutus/aboutus.component";
 
-import { HomeComponent } from 'src/app/pages/home/home.component';
-import { LogoutComponent } from 'src/app/pages/logout/logout.component';
-import { LoginComponent } from '../../pages/login/login.component';
+import { HomeComponent } from "src/app/pages/home/home.component";
+import { LogoutComponent } from "src/app/pages/logout/logout.component";
+import { LoginComponent } from "../../pages/login/login.component";
 
 export const AuthLayoutRoutes: Routes = [
-    { path: 'home',           component: HomeComponent },
-    { path: 'login',          component: LoginComponent },
-    { path: 'logout',       component: LogoutComponent }
+  { path: "home", component: HomeComponent },
+  { path: "login", component: LoginComponent },
+  { path: "logout", component: LogoutComponent },
+  { path: "aboutus", component: AboutusComponent },
 ];
