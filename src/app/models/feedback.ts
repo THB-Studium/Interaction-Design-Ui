3cb619f04893031830106c28ba4e6412141a3c4c
Baudouin Keunne <keunne.baudoin@yahoo.fr>
2022-01-24 20:54:52 +0100

    implemen feedback

--- src/app/models/feedback.ts
+++ src/app/models/feedback.ts
@@ -1,7 +1,8 @@
 export class Feedback {
   public id:string;
   public description: string;
-  public bild: string;
+  public bild: any;
   public veroefentlich: boolean;
   public autor: string;
+  public realImage?: any;
 }
