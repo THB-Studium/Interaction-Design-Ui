a10024c112954aa6d81448a296eadce43ad0904c
steve ngalamo <steve.ngalamo@gematik.de>
2022-02-06 13:29:27 +0100

    #32- Fakten check in More info page - fixed!

--- src/app/pages/learn-more/learn-more.component.ts
+++ src/app/pages/learn-more/learn-more.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewChecked, Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 
 import { CountriesColors } from "../../shared/datas/countries-colors";
@@ -18,7 +18,7 @@ import { Country } from "../../models/country";
   templateUrl: './learn-more.component.html',
   styleUrls: ['./learn-more.component.css']
 })
-export class LearnMoreComponent implements OnInit, AfterViewChecked, OnDestroy {
+export class LearnMoreComponent implements OnInit {
   countries: Array<any> = []
   tripOffers: Array<any> = []
   currentLand: Country
@@ -34,7 +34,6 @@ export class LearnMoreComponent implements OnInit, AfterViewChecked, OnDestroy {
   fontColor: any
   matCardShadow: any
   matCardShadowHighlight: any
-  matCardHeight: any
 
   panelOpenState = false;
   anmeldeFristVorbei = false;
@@ -56,15 +55,6 @@ export class LearnMoreComponent implements OnInit, AfterViewChecked, OnDestroy {
 
   ngOnInit(): void {
     this.setCurrentLandAndTO();
-  }
-
-  ngAfterViewChecked(): void {
-    const height1 = document.getElementById('mat-card-group-1')?.getBoundingClientRect()?.height
-    const height2 = document.getElementById('mat-card-group-2')?.getBoundingClientRect()?.height
-
-    if (height1 && height2) {
-      this.matCardHeight = height1 > height2 ? { 'height.px': height1 } : { 'height.px': height2 }
-    }
   }
 
   ngOnDestroy(): void {
