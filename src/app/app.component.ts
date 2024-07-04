590fd861a52ed275b0c400458874562428fca278
Steve Ngalamo <egoue2001@gmail.com>
2024-07-04 04:37:00 +0200

    chore: migration to standalone - step 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { ChatBotComponent } from './components/chat-bot/chat-bot.component';
+import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [RouterOutlet, ScrollToTopComponent, ChatBotComponent]
 })
 export class AppComponent {
   title: string = 'argon-dashboard-angular';
